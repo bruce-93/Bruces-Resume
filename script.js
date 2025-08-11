4050063df44daf3f95f66f330955ffd34046961c
bruce-93 <105735613+bruce-93@users.noreply.github.com>
2025-08-11 22:09:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@
       const blob = new Blob([document.documentElement.outerHTML], {type: 'application/msword'});
       const url = URL.createObjectURL(blob);
       const a = document.createElement('a');
-      a.href = url; a.download = 'Bruces-Resume/Bruce_Ngixi_Full_Stack_Developer_CV.docx';
+      a.href = url; a.download = 'Bruces-Resume/Bruce_Ngixi_Full_Stack_Developer_CV';
       document.body.appendChild(a);
       a.click();
       a.remove();
